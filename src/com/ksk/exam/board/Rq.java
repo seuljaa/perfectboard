0d5e298f73ab075b5ee9b9df64e1752abfefcbdd
seuljaa <seuljaa940503@gmail.com>
2021-12-03 10:43:30 +0900

    클래스분리및함수분리

---
+++ src/com/ksk/exam/board/Rq.java
@@ -0,0 +1,28 @@
+package com.ksk.exam.board;
+
+import java.util.Map;
+
+public class Rq {
+  private String url;
+  private String urlPath;
+  private Map<String, String> params;
+
+  // 필드추가가능
+
+  // 수정가능
+  Rq(String url) {
+    this.url = url;
+    urlPath = Util.getUrlPathFromUrl(this.url);
+    params = Util.getParamsFromUrl(this.url);
+  }
+
+  // 수정가능, if문 금지
+  public Map<String, String> getParams() {
+    return params;
+  }
+
+  // 수정가능, if문 금지
+  public String getUrlPath() {
+    return urlPath;
+  }
+}
